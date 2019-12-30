a2b3b3c09ba88f2bd8adf289dc50f4f31a730048
Reem H <reemhamoui92@gmail.com>
2019-12-30 14:19:51 -0500

    trying to render pokemon Ids onto page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PokemonCard from './PokemonCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: [],
+      pokemonName: "",
+      pokemonID: [],
+      introRegion: "",
+      typeOne: "",
+      typeTwo: "",
+    }
+  }
+  
+  componentDidMount() {
+    
+      axios({
+        method:'GET',
+        url: `https://pokeapi.co/api/v2/pokemon/?limit=20`,
+        dataResponse: 'json',
+      })
+        .then((data) => {
+          console.log(data.data.results)          
+          
+          
+          this.setState({
+            result: data.data.results
+          })
+
+          this.state.result.map((fetchInfo) => {
+            axios({
+                method: 'GET',
+                url: `${fetchInfo.url}`,
+                dataResponse: 'json',
+              })
+              .then( (dataTwo) =>{
+                console.log('this is the data of the second axios call', dataTwo)
+                console.log(dataTwo.data.id)
+
+                this.setState({
+                  pokemonID: dataTwo.data.id
+                })
+
+                console.log("seeing the ID state",this.state.pokemonID)
+              });
+            
+          })
+        });
+  }
+
+    render() {
+        return (
+            <div className="App">
+              
+            <h1>Pokédex!</h1>
+            
+            <h3>{this.state.pokemonID}</h3> 
+            {this.state.result.map(function(pokeName) {
+              return (
+                <h3>{pokeName.name}</h3>
+              ) 
+
+              })}
+            
+            {/* <PokemonCard /> */}
+            
+            
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/PokemonCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PokemonCard extends Component {
+    render() {
+        return (
+            <div className="App">
+                    
+            </div>
+        );
+    }
+}
+
+export default PokemonCard
